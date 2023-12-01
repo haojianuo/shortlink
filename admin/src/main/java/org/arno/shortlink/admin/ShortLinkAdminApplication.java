ee7d1061f26ea0015185683cbea452fbefc87695
Arno <1945374160@qq.com>
2023-12-01 21:50:08 +0800

    项目和子模块初始化

---
+++ admin/src/main/java/org/arno/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,12 @@
+package org.arno.shortlink.admin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
