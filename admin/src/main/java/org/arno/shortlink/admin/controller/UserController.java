d414d5351c8ff06d6af6bbbedc4f13c496cdc550
Arno <1945374160@qq.com>
2023-12-05 11:36:18 +0800

    定义错误码

--- admin/src/main/java/org/arno/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/arno/shortlink/admin/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.arno.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.arno.shortlink.admin.common.convention.result.Result;
+import org.arno.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.arno.shortlink.admin.dto.response.UserResponseDTO;
 import org.arno.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
     /**
      * 根据用户名获取用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserResponseDTO getUserByUsername(@PathVariable("username") String username) {
-
-        return userService.getUserByUsername(username);
+    public Result<UserResponseDTO> getUserByUsername(@PathVariable("username") String username) {
+        UserResponseDTO userResponseDTO = userService.getUserByUsername(username);
+        if (userResponseDTO == null) {
+            return new Result<UserResponseDTO>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
+        } else {
+            return new Result<UserResponseDTO>().setCode("0").setData(userResponseDTO);
+        }
     }
 }
