c72fa43b53dcd3e6e1c3addc20a769954af48065
Arno <1945374160@qq.com>
2023-12-14 17:13:02 +0800

    修改小bug

--- admin/src/main/java/org/arno/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/arno/shortlink/admin/controller/UserController.java
@@ -1,6 +1,8 @@
 package org.arno.shortlink.admin.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.arno.shortlink.admin.common.convention.result.Result;
+import org.arno.shortlink.admin.common.convention.result.Results;
 import org.arno.shortlink.admin.dto.response.UserResponseDTO;
 import org.arno.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,8 @@ public class UserController {
      * 根据用户名获取用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public UserResponseDTO getUserByUsername(@PathVariable("username") String username) {
+    public Result<UserResponseDTO> getUserByUsername(@PathVariable("username") String username) {
 
-        return userService.getUserByUsername(username);
+        return Results.success(userService.getUserByUsername(username));
     }
 }
--- admin/src/main/java/org/arno/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/arno/shortlink/admin/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package org.arno.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.arno.shortlink.admin.common.convention.exception.ClientException;
+import org.arno.shortlink.admin.common.enums.UserErrorCodeEnum;
 import org.arno.shortlink.admin.dao.entity.UserDO;
 import org.arno.shortlink.admin.dao.mapper.UserMapper;
 import org.arno.shortlink.admin.dto.response.UserResponseDTO;
@@ -18,6 +20,9 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(queryWrapper);
+        if (userDO == null) {
+            throw new ClientException(UserErrorCodeEnum.USER_NULL);
+        }
         UserResponseDTO result = new UserResponseDTO();
         BeanUtils.copyProperties(userDO, result);
         return result;
