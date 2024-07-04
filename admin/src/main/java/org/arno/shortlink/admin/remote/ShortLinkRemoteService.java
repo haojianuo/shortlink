f754166db9f348d964e8a2236e5f120c3544fb60
Arno <1945374160@qq.com>
2024-07-04 18:10:18 +0800

    feature:短链接后管开发短链接修改功能

--- admin/src/main/java/org/arno/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/org/arno/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -7,6 +7,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.arno.shortlink.admin.common.convention.result.Result;
 import org.arno.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
 import org.arno.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import org.arno.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
@@ -19,11 +20,13 @@ import java.util.Map;
  * 短链接平台远程调用服务
  */
 public interface ShortLinkRemoteService {
-    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam){
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
         String resultBodyStr = HttpUtil.post("http://127.0.0.1:7010/api/short-link/v1/create", JSON.toJSONString(requestParam));
         return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
-    };
+    }
+
+    ;
 
     default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
         Map<String, Object> requestMap = new HashMap<>();
@@ -41,5 +44,9 @@ public interface ShortLinkRemoteService {
         String resultPageStr = HttpUtil.get("http://127.0.0.1:7010/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
         });
+    }
+
+    default void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        HttpUtil.post("http://127.0.0.1:7010/api/short-link/v1/update", JSON.toJSONString(requestParam));
     }
 }
