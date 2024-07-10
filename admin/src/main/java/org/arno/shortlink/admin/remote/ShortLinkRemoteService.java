b1f094ac960399ef035bece1ece1d54ccd85a77d
Arno <1945374160@qq.com>
2024-07-10 16:38:37 +0800

    开发获取短链接目标网站标题接口

--- admin/src/main/java/org/arno/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/org/arno/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -11,6 +11,7 @@ import org.arno.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import org.arno.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.HashMap;
 import java.util.List;
@@ -43,6 +44,18 @@ public interface ShortLinkRemoteService {
         requestMap.put("requestParam", requestParam);
         String resultPageStr = HttpUtil.get("http://127.0.0.1:7010/api/short-link/v1/count", requestMap);
         return JSON.parseObject(resultPageStr, new TypeReference<>() {
+        });
+    }
+
+    /**
+     * 根据 URL 获取标题
+     *
+     * @param url 目标网站地址
+     * @return 网站标题
+     */
+    default Result<String> getTitleByUrl(@RequestParam("url") String url) {
+        String resultStr = HttpUtil.get("http://127.0.0.1:7010/api/short-link/v1/title?url=" + url);
+        return JSON.parseObject(resultStr, new TypeReference<>() {
         });
     }
 
