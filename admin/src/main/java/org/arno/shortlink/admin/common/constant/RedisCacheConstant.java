d242c440275ca12c72a0c1de188d0db2d2157dc9
Arno <1945374160@qq.com>
2024-09-11 21:50:53 +1000

    限制最大分组数：20

--- admin/src/main/java/org/arno/shortlink/admin/common/constant/RedisCacheConstant.java
+++ admin/src/main/java/org/arno/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -4,5 +4,13 @@ package org.arno.shortlink.admin.common.constant;
  * 短链接后管 Redis 缓存常量类
  */
 public class RedisCacheConstant {
+    /**
+     * 用户注册分布式锁
+     */
     public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register:";
+
+    /**
+     * 分组创建分布式锁
+     */
+    public static final String LOCK_GROUP_CREATE_KEY = "short-link:lock_group-create:%s";
 }
